1f6103ebb5ffd4cf2ae25295cc63f416232fe43f
由风 <youfeng11@outlook.com>
2025-12-23 13:31:06 +0800

    chore: 修复

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,32 +6,16 @@ pluginManagement {
     gradlePluginPortal()
     google()
     mavenCentral()
-    maven { setUrl("https://maven.aliyun.com/repository/central") }
-    maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-    maven { setUrl("https://maven.aliyun.com/repository/google") }
-    maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-    maven { setUrl("https://maven.aliyun.com/repository/public") }
     maven { setUrl("https://jitpack.io") }
-    maven { setUrl("https://maven.aliyun.com/nexus/content/groups/public/") }
-    maven { setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
-    maven { setUrl("https://maven.scijava.org/content/repositories/public/") }
-    maven { setUrl("https://androidx.dev/storage/compose-compiler/repository/") }
   }
 }
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     google()
     mavenCentral()
     maven("https://jitpack.io")
-    maven { setUrl("https://maven.aliyun.com/repository/central") }
-    maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-    maven { setUrl("https://maven.aliyun.com/repository/google") }
-    maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-    maven { setUrl("https://maven.aliyun.com/repository/public") }
-    maven { setUrl("https://maven.scijava.org/content/repositories/public/") }
-    maven { setUrl("https://androidx.dev/storage/compose-compiler/repository/") }
   }
 }
 
